17fa25ab47d3cbeea2c42acc5c3b19d06db3d3a3
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-06 21:51:52 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,18 @@
 class Project
   
   attr_reader :title
+  @@all = []
   
+  def initialize(title)
+    @title = title
+    @@all = self
+  end
+  
+  def add_backer(backer)
+    ProjectBacker.new(self, backer)
+  end
+  
+  def self.all
+    @@all
+  end
 end
